b5ffc1b363acce3a2041a1de5dce3bf815179ea8
pikann <hvhai22@gmail.com>
2021-12-07 16:09:44 +0700

    feat: improve get presigned URL API

--- src/util/s3-presigned-url.ts
+++ src/util/s3-presigned-url.ts
@@ -1,12 +1,24 @@
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
-import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { v4 as uuidv4 } from 'uuid';
 
 export default async () => {
-  const client = new S3Client({ region: process.env.AWS_S3_REGION });
-  const command = new GetObjectCommand({
-    Bucket: 'mammam-photo-video-bucket-dev',
-    Key: `test-object-${Math.ceil(Math.random() * 10 ** 10)}`,
+  const bucketName = 'mammam-photo-video-bucket-dev';
+  const client = new S3Client({
+    region: process.env.AWS_S3_REGION,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY,
+      secretAccessKey: process.env.AWS_SECRET_KEY,
+    },
   });
-  const url = await getSignedUrl(client, command, { expiresIn: 3600 });
-  return url;
+  const fileName = uuidv4();
+  const command = new PutObjectCommand({
+    Bucket: bucketName,
+    Key: fileName,
+  });
+  const presignedUrl = await getSignedUrl(client, command, { expiresIn: 100 });
+  return {
+    presignedUrl,
+    imageUrl: `https://${bucketName}.s3.ap-southeast-1.amazonaws.com/${fileName}`,
+  };
 };
