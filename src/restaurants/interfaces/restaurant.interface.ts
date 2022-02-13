850c42a886d3ac81aa6ac81d282a510493021e62
pikann <hvhai22@gmail.com>
2022-02-13 17:51:01 +0700

    feat: implement address to restaurant schema

--- src/restaurants/interfaces/restaurant.interface.ts
+++ src/restaurants/interfaces/restaurant.interface.ts
@@ -5,6 +5,7 @@ export interface IRestaurant extends Document {
   readonly name: string;
   readonly bio: string;
   readonly avatar: string;
+  readonly address: string;
   readonly admin: string;
   readonly latitude: number;
   readonly longitude: number;
